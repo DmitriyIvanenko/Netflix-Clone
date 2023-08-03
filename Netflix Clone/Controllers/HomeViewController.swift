93d011104624f0739690c01fb8997242e00e69e1
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-08-03 12:36:25 +0300

    2. Tabbar
    1. Adding new main ViewControllers
    2. Setting up Tabbar

---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Dmytro Ivanenko on 03.08.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controllers/UpcommingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpcommingViewController.swift
+//  Netflix Clone
+//
+//  Created by Dmytro Ivanenko on 03.08.2023.
+//
+
+import UIKit
+
+class UpcommingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+    }
+
+
+}
