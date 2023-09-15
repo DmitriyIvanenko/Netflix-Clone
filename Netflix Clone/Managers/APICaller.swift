149cdac4891edde6668b5e347cc597d026cebd3f
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-09-15 11:51:28 +0300

    API Caller bug fix

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -16,14 +16,13 @@ class APICaller {
     static let shared = APICaller()
     
     func getTrandinMovies(comletion: @escaping (String) -> Void) {
-        guard let url = URL(string: "\(Constants.baseURL)/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
-        
-        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {return}
             
             do {
                 let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
-
+                print(results)
             } catch {
                 print(error.localizedDescription )
             }
@@ -31,4 +30,3 @@ class APICaller {
         task.resume()
     }
 }
-
